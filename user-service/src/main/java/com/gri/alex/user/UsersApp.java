add1d0881d811877aaf6e72543c44f33dd53c43c
Alex Gridasov <alexgridasov@users.noreply.github.com>
2020-10-18 19:32:58 +0300

    Add user repositories and spring dependencies

---
+++ user-service/src/main/java/com/gri/alex/user/UsersApp.java
@@ -0,0 +1,13 @@
+package com.gri.alex.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersApp.class, args);
+    }
+
+}
---
+++ user-service/src/main/java/com/gri/alex/user/domain/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.gri.alex.user.domain.repository;
+
+import java.util.Collection;
+
+public interface UserRepository<Booking, String> extends Repository<Booking, String> {
+
+    boolean containsName(String name);
+
+    Collection<Booking> findByName(String name) throws Exception;
+}
