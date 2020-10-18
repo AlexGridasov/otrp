8ec709dfa64ab1b7f0fba372d1a0d001e1a55499
Alex Gridasov <alexgridasov@users.noreply.github.com>
2020-10-18 19:21:00 +0300

    Add user entities

---
+++ user-service/src/main/java/com/gri/alex/user/domain/model/entity/BaseEntity.java
@@ -0,0 +1,17 @@
+package com.gri.alex.user.domain.model.entity;
+
+public abstract class BaseEntity<T> extends Entity<T> {
+
+    private boolean isModified;
+
+    public BaseEntity(T id, String name) {
+        super.id = id;
+        super.name = name;
+        isModified = false;
+    }
+
+    public boolean isModified() {
+        return isModified;
+    }
+
+}
