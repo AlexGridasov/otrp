10186ec3a683143f512c84c3d6fdf9cbaa7938c9
Alex Gridasov <alexgridasov@users.noreply.github.com>
2020-10-18 17:15:34 +0300

    Add restaurant repositories and spring dependencies

--- restaurant-service/src/main/java/com/gri/alex/restaurant/domain/model/entity/Restaurant.java
+++ restaurant-service/src/main/java/com/gri/alex/restaurant/domain/model/entity/Restaurant.java
@@ -1,27 +1,47 @@
 package com.gri.alex.restaurant.domain.model.entity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Restaurant extends BaseEntity<String> {
 
     private List<Table> tables;
+    private String address;
 
-    public Restaurant(String name, String id, List<Table> tables) {
+    public Restaurant(String name, String id, String address, List<Table> tables) {
         super(id, name);
+        this.address = address;
         this.tables = tables;
     }
 
-    public void setTables(List<Table> tables) {
-        this.tables = tables;
+    private Restaurant(String name, String id) {
+        super(id, name);
+        this.tables = new ArrayList<>();
+    }
+
+    public static Restaurant getDummyRestaurant() {
+        return new Restaurant(null, null);
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 
     public List<Table> getTables() {
         return tables;
     }
 
+    public void setTables(List<Table> tables) {
+        this.tables = tables;
+    }
+
     @Override
     public String toString() {
-        return "{id: " + id + ", name: "
-                + name + ", tables: " + tables + "}";
+        return String.format("{id: %s, name: %s, address: %s, tables: %s}",
+                this.getId(), this.getName(), this.getAddress(), this.getTables());
     }
 }
