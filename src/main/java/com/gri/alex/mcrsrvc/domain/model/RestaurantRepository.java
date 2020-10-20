108c1cd5e55513450988da2cbc8e3775fc7737b3
Alex Gridasov <alexgridasov@users.noreply.github.com>
2020-10-20 15:47:26 +0300

    Delete old code

--- src/main/java/com/gri/alex/mcrsrvc/domain/model/RestaurantRepository.java
+++
@@ -1,6 +0,0 @@
-package com.gri.alex.mcrsrvc.domain.model;
-
-public interface RestaurantRepository<Restaurant, String> extends Repository<Restaurant, String> {
-
-    boolean containsName(String name);
-}
