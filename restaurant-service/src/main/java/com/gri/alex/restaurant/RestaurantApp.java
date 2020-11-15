be0c05b4109c52de3409ee5932496b0fb0b32805
Alex Gridasov <alexgridasov@users.noreply.github.com>
2020-11-15 19:30:13 +0200

    Integrate restaurant-service with eureka-server

--- restaurant-service/src/main/java/com/gri/alex/restaurant/RestaurantApp.java
+++ restaurant-service/src/main/java/com/gri/alex/restaurant/RestaurantApp.java
@@ -2,8 +2,10 @@ package com.gri.alex.restaurant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RestaurantApp {
 
     public static void main(String[] args) {
