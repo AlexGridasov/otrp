f352be780e9f09179c89fcb32bcc15e05fbfdbb4
Alex Gridasov <alexgridasov@users.noreply.github.com>
2020-11-29 19:30:33 +0200

    booking-service: add OpenfeignClientSample

--- booking-service/src/main/java/com/gri/alex/booking/BookingApp.java
+++ booking-service/src/main/java/com/gri/alex/booking/BookingApp.java
@@ -1,7 +1,9 @@
 package com.gri.alex.booking;
 
 import com.gri.alex.booking.common.httpclient.UserRestClient;
+import com.gri.alex.booking.common.openfeign.UserFeignClient;
 import com.gri.alex.booking.common.resttemplate.UserRestTemplate;
+import com.gri.alex.booking.domain.valueobject.UserVO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +13,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.stereotype.Component;
 
-@SpringBootApplication
+@EnableFeignClients
 @EnableEurekaClient
+@SpringBootApplication
 public class BookingApp {
 
     public static void main(String[] args) {
@@ -100,5 +104,69 @@ class Java11HttpClientSample implements CommandLineRunner {
         httpClient.deleteUser();
         LOG.info("\n\nRetrieve user again to check if deleted user still exists");
         httpClient.getUser();
+    }
+}
+
+@Component
+@ConditionalOnProperty(prefix = "command.autorun", name = "enabled",
+        havingValue = "true", matchIfMissing = true)
+class OpenfeignClientSample implements CommandLineRunner {
+
+    private static final Logger LOG = LoggerFactory.getLogger(OpenfeignClientSample.class);
+
+    @Autowired
+    private UserFeignClient userFeignClient;
+
+    @Override
+    public void run(String... strings) {
+        LOG.info("Creating new user");
+        UserVO userVO = new UserVO();
+        userVO.setId("5");
+        userVO.setName("Y User");
+        userVO.setAddress("Y Address");
+        userVO.setCity("Y City");
+        userVO.setPhoneNo("1234567890");
+        try {
+            UserVO newUser = userFeignClient.postUser(userVO);
+            assert newUser.getId().equals("5");
+        } catch (Exception e) {
+            LOG.error(e.getMessage());
+        }
+
+        LOG.info("\n\nUpdate newly created user");
+        userVO = new UserVO();
+        userVO.setId("5");
+        userVO.setName("Y User 1");
+        userVO.setAddress("Y Address 1");
+        userVO.setCity("Y City 1");
+        userVO.setPhoneNo("1234567890");
+        try {
+            userFeignClient.putUser(5, userVO);
+        } catch (Exception e) {
+            LOG.error(e.getMessage());
+        }
+
+        LOG.info("\n\nRetrieve users again to check if newly created object got updated");
+        try {
+            userFeignClient.getUser("y")
+                    .forEach((UserVO user) -> LOG.info("GET /v1/user --> {}", user));
+        } catch (Exception e) {
+            LOG.error(e.getMessage());
+        }
+
+        LOG.info("\n\nDelete newly created user");
+        try {
+            userFeignClient.deleteUser(5);
+        } catch (Exception e) {
+            LOG.error(e.getMessage());
+        }
+
+        LOG.info("\n\nRetrieve users again to check if deleted user still exists");
+        try {
+            userFeignClient.getUser("y")
+                    .forEach((UserVO user) -> LOG.info("GET /v1/user --> {}", user));
+        } catch (Exception e) {
+            LOG.error(e.getMessage());
+        }
     }
 }
---
+++ booking-service/src/main/java/com/gri/alex/booking/common/openfeign/UserFeignClient.java
@@ -0,0 +1,29 @@
+package com.gri.alex.booking.common.openfeign;
+
+import com.gri.alex.booking.domain.valueobject.UserVO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Collection;
+
+@Component
+@FeignClient("user-service")
+public interface UserFeignClient {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/v1/user")
+    Collection<UserVO> getUser(@RequestParam("name") String name) throws Exception;
+
+    @RequestMapping(method = RequestMethod.POST, value = "/v1/user")
+    UserVO postUser(@RequestBody UserVO user) throws Exception;
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/v1/user/{id}")
+    void putUser(@PathVariable("id") long id, @RequestBody UserVO user) throws Exception;
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/v1/user/{id}")
+    void deleteUser(@PathVariable("id") long id) throws Exception;
+}
